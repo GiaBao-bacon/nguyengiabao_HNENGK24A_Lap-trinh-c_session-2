8e7e29a453a736aab97d0e3227320127a521e55b
GiaBao-bacon <nguyengiabao102006bacon@gmail.com>
2025-04-24 12:04:43 +0700

    upload bài 1

---
+++ Untitled14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	
+	int age =25;//int kieu so nguyen 
+	float cm =1.75;//float kieu so thuc  
+	double pi =3.14;//double cung kieu so thuc nhung do chinh xac cao hon 
+	char real = 'A';//char kieu ki tu 
+	
+	
+	return 0;
+	
+}
