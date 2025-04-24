b777aef7938f70cb66ec8ad2ba8844abe3e490eb
GiaBao-bacon <nguyengiabao102006bacon@gmail.com>
2025-04-24 12:00:09 +0700

    upload bài 4

---
+++ Untitled11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+    int canh = 5;
+    int chuvi = 4 * canh;
+    int dientich = canh * canh; 
+
+    printf("Canh hinh vuong la: %d\n", canh);
+    printf("Chu vi hinh vuong la: %d\n", chuvi);
+    printf("Dien tich hinh vuong la: %d\n", dientich);
+
+    return 0;
+}
