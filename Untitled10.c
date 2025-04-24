dcb680857f1013c4d1689283b80cb71a69263a08
GiaBao-bacon <nguyengiabao102006bacon@gmail.com>
2025-04-24 11:58:52 +0700

    upload bài 3

---
+++ Untitled10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+	int num1 =30;
+	int num2 =20;
+	int total= num1 + num2;
+	int hieu= num1 -num2;
+	int tich= num1 *num2;
+	int thuong= num1 /num2;
+	printf("tong cua hai so tren la: %d\n", total);
+	printf("hieu cua hai so tren la: %d\n", hieu);
+	printf("tich cua hai so tren la: %d\n", tich);
+	printf("thuong cua hai so tren la: %d\n", thuong);
+	return 0;
+}
